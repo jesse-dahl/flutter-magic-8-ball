d47fadf6feebeff752a4378661d8757b71e55603
jesse-dahl <jddahl199@gmail.com>
2019-06-27 16:59:08 -0600

    added an appbar and statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Magic 8 Ball'),
+            backgroundColor: Colors.blue[800],
+          ),
+          body: BallPage(),
+        ),
       ),
     );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
